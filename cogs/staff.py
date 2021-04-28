797285bbfef817254a1b04779f97655b3a7c7103
garfied <tvach2167@gmail.com>
2021-04-28 07:32:08 +0000

    Added tempmute and tempban commands

--- cogs/staff.py
+++ cogs/staff.py
@@ -1,4 +1,4 @@
-import discord, config, os, time
+import discord, config, os, sys, time, timeit, asyncio
 from discord.ext import commands
 from pretty_help import PrettyHelp
 
@@ -6,28 +6,49 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(help='Reload all of the bot\'s cogs (only Music_Dude can use this)', aliases=['reload'])
+    @commands.command(help='Reload all of the bot\'s cogs (only Music_Dude can use this)')
+    async def reload(self, ctx):
+        if ctx.author.id == config.ownerID:
+            em=discord.Embed(
+                title='Reloading all cogs. . .',
+                description='Allow up to 5 seconds',
+                color=discord.Color.green()
+            )
+            msg = await ctx.send(embed=em)
+            await self.bot.change_presence(activity=discord.Game(name="Restarting. . ."))
+            print('Bot is reloading. . .')
+            tic = time.perf_counter()
+
+            for filename in os.listdir('./cogs'):
+                if filename.endswith('.py') and not filename.startswith('_'):
+                    filename = filename[:-3]
+                    print(f'Reloading cog {filename}')
+                    em.add_field(name=filename, value=f'Reloaded cog {filename}')
+                    self.bot.reload_extension(f'cogs.{filename}')
+
+            toc = time.perf_counter()
+
+            em.add_field(name='Finished reloading bot', value=f'Reloaded all cogs in {toc-tic:0.4f} seconds', inline=False)
+            await msg.edit(embed=em)
+            await self.bot.change_presence(activity=config.activity)
+            print('Bot is back up!')
+
+        else:
+            await ctx.send(f'Only <@{config.ownerID}> can use this command to prevent abuse!')
+
+    @commands.command(help='Restart the entire bot (only Music_Dude can use this)')
     async def restart(self, ctx):
         if ctx.author.id == config.ownerID:
             with ctx.typing():
                 em=discord.Embed(
-                    title='Restarting. . . Allow up to 5 seconds'
+                    title='Restarting. . .',
+                    description='Allow up to 5 seconds',
+                    color=discord.Color.green()
                 )
                 msg = await ctx.send(embed=em)
                 await self.bot.change_presence(activity=discord.Game(name="Restarting. . ."))
                 print('Bot is restarting. . .')
-
-                for filename in os.listdir('./cogs'):
-                    if filename.endswith('.py') and not filename.startswith('_'):
-                        filename = filename[:-3]
-                        print(f'Reloading cog {filename}')
-                        self.bot.reload_extension(f'cogs.{filename}')
-
-                em=discord.Embed(
-                    title='Bot is back up!'
-                )
-                await msg.edit(embed=em)
-                await self.bot.change_presence(activity=config.activity)
+                os.execl(sys.executable, 'python3', './main.py', *sys.argv[1:])
             print('Bot is back up!')
 
         else:
@@ -49,16 +70,32 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
             await ctx.send(embed=stealEm)
 
     @commands.command(help='Changes the bot\'s nickname')
-    @commands.has_permissions(administrator=True)
-    async def nick(self, ctx, *New_Name):
+    @commands.has_permissions(manage_nicknames=True)
+    async def botnick(self, ctx, *New_Name):
         New_Name = ' '.join(New_Name)
         try:
             await ctx.guild.get_member(self.bot.user.id).edit(nick=New_Name)
             await ctx.send(f'Successfully changed nickname to \"{New_Name}\"')
         except discord.HTTPException:
-            await ctx.send('Couldn\'t set nickname to that. The new name must be 32 or less characters in length.')
+            await ctx.send('Couldn\'t set nickname to that. Check that the new name is 32 or less characters in length and doesn\'t contain special characters.'
+            )
 
-    @commands.command(help='Mute a member', pass_context=True)
+    @commands.command(help='Changes a member\'s nickname')
+    @commands.has_permissions(manage_nicknames=True)
+    async def nick(self, ctx, user : discord.Member=None, *New_Name):
+        if user == None:
+            user = ctx.author
+
+        New_Name = ' '.join(New_Name)
+        try:
+            await user.edit(nick=New_Name)
+            await ctx.send(f'Successfully changed {user}\'s nickname to \"{New_Name}\"')
+        except discord.Forbidden:
+            await ctx.send('I can\'t nickname that user 😢')
+        except discord.HTTPException:
+            await ctx.send('Couldn\'t set nickname to that. Check that the new name is 32 or less characters in length and doesn\'t contain special characters.')
+
+    @commands.command(help='Mute a member indefinitely', pass_context=True)
     @commands.has_permissions(manage_roles=True)
     async def mute(self, ctx, user: discord.Member=None):
         if user == None:
@@ -74,9 +111,47 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
 
             em = discord.Embed(
                 title=f'✅ Muted {user}',
-                color= 0x74B353
+                color= discord.Color.green()
             )
             await ctx.send(embed=em)
+        except discord.Forbidden:
+            await ctx.send('I can\'t mute that user 😢')
+    
+    @commands.command(help='Mute a member temporarily', pass_context=True)
+    @commands.has_permissions(manage_roles=True)
+    async def tempmute(self, ctx, user: discord.Member=None, time=None):
+        if user == None:
+            await ctx.send('You must provide a user to mute!')
+            return
+
+        time_convert = {"s":1, "m":60, "h":3600,"d":86400}
+        if not time[-1] in time_convert:
+            await ctx.reply('Please specify a value of s (seconds), m (minutes), h (hours), or d (days)')
+            return
+        time = int(time[:-1]) * time_convert[time[-1]]
+
+        if user.top_role > ctx.author.top_role and ctx.author.id != config.ownerID:
+            await ctx.send('You are not high enough in the role hierarchy to mute that user!')
+            return
+
+        try:
+            await user.add_roles(ctx.guild.get_role(config.mute_role))
+
+            em = discord.Embed(
+                title=f'✅ Muted {user} for {time} seconds',
+                color= discord.Color.green()
+            )
+            await ctx.send(embed=em)
+
+            await asyncio.sleep(time)
+            await user.remove_roles(ctx.guild.get_role(config.mute_role))
+
+            em = discord.Embed(
+            title=f'✅ {user} was unmuted after {time} seconds',
+            color= discord.Color.green()
+            )
+            await ctx.send(embed=em)
+
         except discord.Forbidden:
             await ctx.send('I can\'t mute that user 😢')
 
@@ -96,7 +171,7 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
 
             em = discord.Embed(
                 title=f'✅ Unmuted {user}',
-                color= 0x74B353
+                color= discord.Color.green()
             )
             await ctx.send(embed=em)
         except discord.Forbidden:
@@ -117,13 +192,49 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
             await user.kick(reason=reason)
             em = discord.Embed(
                 title=f'✅ Successfully kicked {user}',
-                color= 0x74B353
+                color= discord.Color.green()
             )
             await ctx.send(embed=em)
         except discord.Forbidden:
             await ctx.send('I can\'t kick that user 😢')
 
-    @commands.command(help='Ban a member', aliases=['yeet'], pass_context=True)
+    @commands.command(help='Ban a member temporarily', aliases=['tempyeet'], pass_context=True)
+    @commands.has_permissions(ban_members=True)
+    async def tempban(self, ctx, user: discord.Member=None, time=None, *, reason=None):
+        if user == None:
+            ctx.send('You must provide a user to ban!')
+            return
+
+        time_convert = {"s":1, "m":60, "h":3600,"d":86400}
+        if not time[-1] in time_convert:
+            await ctx.reply('Please specify a value of s (seconds), m (minutes), h (hours), or d (days)')
+            return
+        time = int(time[:-1]) * time_convert[time[-1]]
+
+        if user.top_role > ctx.author.top_role and ctx.author.id != config.ownerID:
+            ctx.send('You are not high enough in the role hierarchy to ban that user!')
+            return
+
+        try:
+            await user.ban(reason=reason)
+            em = discord.Embed(
+                title=f'✅ Successfully banned {user} for {time} seconds',
+                color= discord.Color.green()
+            )
+            await ctx.send(embed=em)
+
+            await asyncio.sleep(time)
+            await user.unban()
+
+            em = discord.Embed(
+                title=f'✅ {user} was unbanned after {time} seconds',
+                color= discord.Color.green()
+            )
+            await ctx.send(embed=em)
+        except discord.Forbidden:
+            await ctx.send('I can\'t ban that user 😢')
+
+    @commands.command(help='Ban a member indefinitely', aliases=['yeet'], pass_context=True)
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user: discord.Member=None, *reason):
         if user == None:
@@ -138,7 +249,7 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
             await user.ban(reason=reason)
             em = discord.Embed(
                 title=f'✅ Successfully banned {user}',
-                color= 0x74B353
+                color= discord.Color.green()
             )
             await ctx.send(embed=em)
         except discord.Forbidden:
@@ -160,13 +271,17 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
                 await ctx.guild.unban(banned_user)
                 em = discord.Embed(
                     title=f'✅ Successfully unbanned {user}',
-                    color= 0x74B353
+                    color= discord.Color.green()
                 )
                 await ctx.send(embed=em)
 
     @commands.command(help='Give a user a role', aliases=['giverole'])
     @commands.has_permissions(manage_roles=True)
-    async def role(self, ctx, user: discord.Member=None, role: discord.Role=None):
+    async def role(self, ctx, user: discord.Member=None, *, role=None):
+        if user==None:
+            await ctx.send('You must provide a role to give that user!')
+            return
+
         if role==None:
             await ctx.send('You must provide a role to give that user!')
             return
@@ -175,18 +290,31 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
             await ctx.send('You are not high enough in the role hierarchy to change roles for that user!')
             return
 
+        if type(role) == str:
+            for server_role in ctx.guild.roles:
+                if (str(role)).lower() in str(server_role).lower():
+                    role = server_role
+        if type(role) == str:
+            em=discord.Embed(
+                title=f'Role not found',
+                description=f'Couldn\'t find a role called \"{role}\".',
+                color=discord.Color.red()
+            )
+            await ctx.send(embed=em)
+            return      
+
         if role in user.roles:
             await user.remove_roles(role)
             em = discord.Embed(
                 title=f'Successfully removed role \"{role.name}\" from {user.name}!',
-                color=0xff0000
+                color=discord.Color.red()
             )
             await ctx.send(embed=em)
         else:
             await user.add_roles(role)
             em = discord.Embed(
                 title=f'Successfully gave role \"{role.name}\" to {user.name}!',
-                color=0x12e00b
+                color=discord.Color.green()
             )
             await ctx.send(embed=em)
 
@@ -203,7 +331,7 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
         await new_channel.edit(position=pos)
         em = discord.Embed(
             description=f'Cloned <#{channel.id}>, creating <#{new_channel.id}>',
-            color=0x12e00b
+            color=discord.Color.green()
         )
         await ctx.send(embed=em)
 
@@ -248,7 +376,7 @@ class Staff(commands.Cog, description='Admin/moderation commands'):
                     await channel.delete(reason=f'Channel deleted by {ctx.author}')
                     em = discord.Embed(
                         title=f'✅ Successfully deleted <#{channel.id}>',
-                        color=0x74B353
+                        color=discord.Color.green()
                     )
                     try:
                         await ctx.send(embed=em)
