cb1b0e55c04dc4d41d3a6f14c911ba81ee3e4c53
garfied <tvach2167@gmail.com>
2021-05-27 14:16:08 -0400

    Fixed timestamp for bot info command

--- cogs/utility.py
+++ cogs/utility.py
@@ -3,6 +3,10 @@ import requests
 import random
 import asyncio
 import config
+import time
+import psutil
+import main
+from datetime import datetime
 from string import ascii_letters, digits
 from colors import colordict
 from urllib.parse import quote
@@ -13,12 +17,35 @@ class Utility(commands.Cog, description='Somewhat useful commands'):
     def __init__(self, bot):
         self.bot = bot
 
+    def getUptime(self):
+        seconds = time.time()-main.startTime
+        min, sec = divmod(seconds, 60)
+        hour, min = divmod(min, 60)
+        return f'{hour:0.0F} hours, {min:0.0F} minutes and {sec:0.0F} seconds'
+
+    def getStartTime(self):
+        value = datetime.fromtimestamp(main.startTime)
+        return value.strftime('%Y-%m-%d %H:%M:%S')
+
     def check_author(self, author):
         def inner_check(message):
             return message.author == author
         return inner_check
 
-    @commands.command(help='Times your typing of words')
+    @commands.command(help='Show information about the bot', aliases=['info', 'botinfo'])
+    async def bot(self, ctx):
+        em = discord.Embed(
+            title='Bot Info',
+            color=discord.Color.random()
+        )
+        em.add_field(name='Uptime', value=self.getUptime(), inline=False)
+        em.add_field(name='Since', value=self.getStartTime(), inline=False)
+        em.add_field(name='System Stats',
+                     value=f'{psutil.cpu_percent()}% CPU usage\n{psutil.virtual_memory().percent}% memory usage', inline=False)
+
+        await ctx.send(embed=em)
+
+    @commands.command(help='Verify yourself in the server')
     async def verify(self, ctx):
         verified_role = ctx.guild.get_role(config.verified_role)
 
@@ -41,13 +68,14 @@ class Utility(commands.Cog, description='Somewhat useful commands'):
             title=f'Verification for {ctx.guild}',
             description='We need to make sure you\'re human',
             color=col
-        
+
         )
         captcha = ''.join(random.choices(ascii_letters + digits, k=5))
-        em.set_image(url=f'https://lingtalfi.com/services/pngtext?text=%20{captcha}%20&color={str(col)[1:]}&size=100')
+        em.set_image(
+            url=f'https://lingtalfi.com/services/pngtext?text=%20{captcha}%20&color={str(col)[1:]}&size=100')
 
         verifyMessage = await ctx.author.send(embed=em)
-        
+
         try:
             answer = await self.bot.wait_for('message', check=self.check_author(ctx.author), timeout=30)
         except asyncio.TimeoutError:
@@ -55,7 +83,7 @@ class Utility(commands.Cog, description='Somewhat useful commands'):
             return
 
         em = discord.Embed(
-            title = f'Successfully verified in {ctx.guild}!',
+            title=f'Successfully verified in {ctx.guild}!',
             description='Have fun!',
             color=col
         )
@@ -72,9 +100,9 @@ class Utility(commands.Cog, description='Somewhat useful commands'):
     @commands.command(help="Show current member count", aliases=["membercount"])
     async def members(self, ctx):
         em = discord.Embed(
-        title=f"Members in {ctx.guild}",
-        description=f"```{ctx.guild.member_count}```",
-        color=discord.Color.blurple()
+            title=f"Members in {ctx.guild}",
+            description=f"```{ctx.guild.member_count}```",
+            color=discord.Color.blurple()
         )
 
         await ctx.send(embed=em)
