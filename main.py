d1c5fd9ab82f4d4ea811dc4ad301700b53a283d8
garfied <tvach2167@gmail.com>
2021-05-27 14:18:23 -0400

    Added starttime attribute to set on bot startup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import config
+import time
 from datetime import datetime
 from discord.ext import commands
 from pretty_help import PrettyHelp
@@ -13,6 +14,7 @@ bot = commands.Bot(
 
 )
 
+startTime = time.time()
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py') and not filename.startswith('X'):
         filename = filename[:-3]
@@ -24,6 +26,5 @@ for filename in os.listdir('./cogs'):
 async def on_ready():
     await bot.change_presence(activity=config.activity)
     print('Bot is ready.')
-
 
 bot.run(config.token)
