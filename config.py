c763c6d817f7d7bbe5f5c4a998e370d6d1e4c61b
Music-Dude <78457240+Music-Dude@users.noreply.github.com>
2021-06-10 18:29:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ token = 'your token here'
 prefix = '!'
 # Bot will listen for commands starting with this prefix
 
-activity = discord.Activity(type=discord.ActivityType.listening, name=f'{prefix}help')
+activity = discord.Activity(type=2, name=f'{prefix}help')
 # Set this to any status the bot should have on start
-# Examples:
-#    discord.Game(name='a game')                                                          >> Playing a game
-#    discord.Streaming(name='stream', url='twitch url')                                   >> Streaming stream
-#    discord.Activity(type=discord.ActivityType.listening, name='a song')                 >> Listening to a song
-#    discord.Activity(type=discord.ActivityType.watching, name='a video')                 >> Watching a video
-#    discord.Activity(type=discord.ActivityType.custom, state='Your custom status')       >> Your custom status
+# Types:
+#   1 - Playing ...
+#   2 - Listening to ...
+#   3 - Watching ...
+#   4 - Streaming (you can set url='') ...
+#   5 - Competing in ...
 
 cat_key = ''
 # Get an api key from https://thecatapi.com/
