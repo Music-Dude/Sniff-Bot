d51bca9862b36d9f03199de934d115086d88170b
garfied <tvach2167@gmail.com>
2021-06-14 15:57:46 -0400

    Added snipe command

--- cogs/triggers.py
+++ cogs/triggers.py
@@ -1,10 +1,16 @@
-import discord, random, time, config, re
+import discord
+import random
+import time
+import config
+import re
 from discord.ext import commands
 from pretty_help import PrettyHelp
+
 
 class Triggers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.snipedMessage = ''
 
     @commands.Cog.listener()
     async def on_message(self, msg):
@@ -16,15 +22,9 @@ class Triggers(commands.Cog):
             await msg.channel.send(f'Use {config.prefix}help for a list of commands')
 
     @commands.Cog.listener()
-    async def on_guild_channel_create(self, channel):
-        if channel.name.startswith('ticket'):
-            em = discord.Embed(
-                title='If you\'re using an executor that doesn\'t support MT-API (KRNL, Fluxus, etc), try the lite version of the script',
-                color = 0x2aa198
-            )
-            em.add_field(name='Lite Script:', value='```lua\nloadstring(game:HttpGet(\'https://raw.githubusercontent.com/2dgeneralspam1/Sniff-Hub/main/sniff%20hub%20lite\')()```')
-            time.sleep(1)
-            await channel.send(embed=em)
+    async def on_message_delete(self, msg):
+        self.bot.snipedMessage = msg
+
 
 def setup(bot):
     bot.add_cog(Triggers(bot))
